406cb28395585e42c433c9ce51c1204dc60de9dd
Gustavo Picon <tabo@tabo.pe>
2010-03-03 22:46:27 -0500

    LPUSH/RPUSH return the new list length

--- redis.c
+++ redis.c
@@ -4029,7 +4029,7 @@ static void pushGenericCommand(redisClient *c, int where) {
     lobj = lookupKeyWrite(c->db,c->argv[1]);
     if (lobj == NULL) {
         if (handleClientsWaitingListPush(c,c->argv[1],c->argv[2])) {
-            addReply(c,shared.ok);
+            addReply(c,shared.czero);
             return;
         }
         lobj = createListObject();
@@ -4042,25 +4042,24 @@ static void pushGenericCommand(redisClient *c, int where) {
         dictAdd(c->db->dict,c->argv[1],lobj);
         incrRefCount(c->argv[1]);
         incrRefCount(c->argv[2]);
+        server.dirty++;
     } else {
         if (lobj->type != REDIS_LIST) {
             addReply(c,shared.wrongtypeerr);
             return;
         }
-        if (handleClientsWaitingListPush(c,c->argv[1],c->argv[2])) {
-            addReply(c,shared.ok);
-            return;
-        }
         list = lobj->ptr;
-        if (where == REDIS_HEAD) {
-            listAddNodeHead(list,c->argv[2]);
-        } else {
-            listAddNodeTail(list,c->argv[2]);
+        if (!handleClientsWaitingListPush(c,c->argv[1],c->argv[2])) {
+            if (where == REDIS_HEAD) {
+                listAddNodeHead(list,c->argv[2]);
+            } else {
+                listAddNodeTail(list,c->argv[2]);
+            }
+            incrRefCount(c->argv[2]);
+            server.dirty++;
         }
-        incrRefCount(c->argv[2]);
     }
-    server.dirty++;
-    addReply(c,shared.ok);
+    addReplySds(c,sdscatprintf(sdsempty(),":%d\r\n",listLength(list)));
 }
 
 static void lpushCommand(redisClient *c) {
